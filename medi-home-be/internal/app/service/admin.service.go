d44bb3969737e82d771776b536a248213aa04978
milopq23 <nhpquy230104@gmail.com>
2025-09-25 17:23:37 +0700

    crud admin

---
+++ medi-home-be/internal/app/service/admin.service.go
@@ -0,0 +1,57 @@
+package service
+
+import(
+	"medi-home-be/internal/app/model"
+	"medi-home-be/internal/app/repository"
+)
+
+type AdminService interface{
+	GetAll() ([]model.Admin,error)
+	GetByID(id uint) (model.Admin,error)
+	Create(admin model.Admin) (model.Admin,error)
+	Patch(id uint, updates map[string]interface{}) (model.Admin,error)
+	Delete(id uint) error
+}
+
+type adminService struct{
+	repo repository.AdminRepository
+}
+
+func NewAdminService(repo repository.AdminRepository) AdminService{
+	return &adminService{repo}
+}
+
+func (s *adminService) GetAll() ([]model.Admin,error){
+	return s.repo.FindAll()
+}
+
+func (s *adminService) GetByID(id uint) (model.Admin,error){
+	return s.repo.FindByID(id)
+}
+
+func (s *adminService) Create(admin model.Admin) (model.Admin,error){
+	return s.repo.Create((admin))
+}
+
+func (s *adminService) Patch(id uint, data map[string] interface{} ) (model.Admin,error){
+	admin, err := s.repo.FindByID(id)
+	if err != nil{
+		return model.Admin{},err
+	}
+	allowedFields := map[string]bool{
+		"phone" :true,
+		"password" : true,
+		"role" :true,
+	}
+	updates := make(map[string] interface{})
+	for k,v := range data{
+		if allowedFields[k]{
+			updates[k] = v
+		}
+	}
+	return s.repo.Patch(uint(admin.AdminID),updates)
+}
+
+func (s *adminService) Delete(id uint ) error{
+	return s.repo.Delete(id)
+}
